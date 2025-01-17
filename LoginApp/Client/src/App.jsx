de677121ab893cdea21ece590e616d40f43e352b
Govind25Sharma <143006104+Govind25Sharma@users.noreply.github.com>
2025-01-17 17:44:43 +0530

    Update App.jsx
    correct proper routing path

--- LoginApp/Client/src/App.jsx
+++ LoginApp/Client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Welcome from "./components/welcome/Welcome.jsx"
 import SignUp from "./components/signUp/SignUp.jsx"
 import LoginIn from "./components/loginIn/LoginIn.jsx"
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<useNavigate to="/welcome" />} />
+        <Route path="/" element={<Navigate to="/welcome" />} />
         <Route path='/welcome' element={<Welcome />}></Route>
         <Route path='/register' element={<SignUp />}></Route>
         <Route path='/login' element={<LoginIn />}></Route>
